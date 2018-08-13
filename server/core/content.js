bf16f31fb8b9532a31949eaa77b0233f40d18f6a
kitajchuk <kitajchuk@gmail.com>
2018-08-13 13:35:25 -0700

    check if csrfToken method exists in content done() method

--- server/core/content.js
+++ server/core/content.js
@@ -58,7 +58,7 @@ const getPage = function ( req, res, listener ) {
         };
         const done = function () {
             context.set({
-                csrf: req.csrfToken(),
+                csrf: req.csrfToken ? req.csrfToken() : null,
                 navi: core.query.cache.navi,
                 site: core.query.cache.site,
                 footer: core.query.cache.footer
