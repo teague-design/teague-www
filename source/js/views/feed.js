91f51b92a24f53d72f26f58d9d4dc0dfb8a7876e
kitajchuk <kitajchuk@gmail.com>
2018-09-04 11:28:55 -0700

    tweak stories page

--- source/js/views/feed.js
+++ source/js/views/feed.js
@@ -19,19 +19,19 @@ export default ( view ) => {
             ${view.json.documents.map(( doc ) => {
                 return `
                     <div class="feed__item">
-                        <div class="feed__cat cms">
-                            <a href="${view.data.page}?category=${doc.data.category}">
-                                <h6>${doc.data.category}</h6>
-                            </a>
-                        </div>
                         <div class="feed__image">
                             <a href="/${doc.type}/${doc.uid}/">
                                 ${viewImage( doc.data.image )}
                             </a>
                         </div>
+                        <div class="feed__cat cms">
+                            <a href="${view.data.page}?category=${doc.data.category}">
+                                <h6>${doc.data.category}</h6>
+                            </a>
+                        </div>
                         <div class="feed__title cms">
                             <a href="/${doc.type}/${doc.uid}/">
-                                ${dom.RichText.asHtml( doc.data.title ).replace( /h1/g, "h4" )}
+                                ${dom.RichText.asHtml( doc.data.title ).replace( /h1/g, "h5" )}
                             </a>
                         </div>
                     </div>
