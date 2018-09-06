00fc926c9c2fadd9d792224803c22b95a9d200b5
kitajchuk <kitajchuk@gmail.com>
2018-09-06 16:49:16 -0700

    make reusable .ac class

--- source/js/views/taxi.js
+++ source/js/views/taxi.js
@@ -16,9 +16,9 @@ export default ( view ) => {
     return `
         <div class="taxi__cats">
             <div class="taxi__label p">Filter by</div>
-            <a class="taxi__cat js-taxi-cat h6 ${params.category ? "" : "is-active"}" href="${view.data.page}">All</a>
+            <a class="taxi__cat js-taxi-cat h6 ac ${params.category ? "" : "is-active"}" href="${view.data.page}">All</a>
             ${cats.map(( cat ) => {
-                return `<a class="taxi__cat js-taxi-cat h6 ${params.category === cat ? "is-active" : ""}" href="#${view.data.page}?category=${cat}" data-value="${cat}">${cat}</a>`;
+                return `<a class="taxi__cat js-taxi-cat h6 ac ${params.category === cat ? "is-active" : ""}" href="#${view.data.page}?category=${cat}" data-value="${cat}">${cat}</a>`;
 
             }).join( "" )}
         </div>
