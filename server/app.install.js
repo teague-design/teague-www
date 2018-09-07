97b70c5ce24159894d0056f4c57b60ff67610346
kitajchuk <kitajchuk@gmail.com>
2018-09-07 11:11:56 -0700

    add pid check for server installs

--- server/app.install.js
+++ server/app.install.js
@@ -4,6 +4,8 @@ const root = __dirname;
 const rootNodeModules = path.join( root, "node_modules" );
 const rootPackageLock = path.join( root, "package-lock.json" );
 const child_process = require( "child_process" );
+const pidLogFile = path.join( root, "pid.log" );
+const config = require( "../clutch.config" );
 
 
 // Note that with `npm@5` there have been some hiccups
@@ -19,15 +21,22 @@ child_process.execSync( "npm install" );
 
 
 // 2.0 Stop `environment` server
-console.log( `Stopping ${process.env.NODE_ENV} server...` );
+if ( fs.existsSync( pidLogFile ) ) {
+    console.log( `Stopping ${process.env.NODE_ENV} server...` );
 
-child_process.execSync( "npm run stop" );
+    child_process.execSync( "npm run stop" );
+
+} else {
+    console.log( `No PID file, skipping stop command...` );
+}
 
 
 // 3.0 Make sure ports are forwarded for node
-console.log( `Forwarding port 80 to port 8000...` );
+if ( config.staging || config.production ) {
+    console.log( `Forwarding port 80 to port 8000...` );
 
-child_process.execSync( "iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8000" );
+    child_process.execSync( "iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8000" );
+}
 
 
 // 4.0 Start `environment` server
