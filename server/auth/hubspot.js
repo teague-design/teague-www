5ab5a9fbc7dea1c760c8252d4dd6f126b70910cf
kitajchuk <kitajchuk@gmail.com>
2018-09-13 13:07:06 -0700

    remove validator package...

--- server/auth/hubspot.js
+++ server/auth/hubspot.js
@@ -10,7 +10,14 @@ const authorization = {
     store: path.join( __dirname, "../../sandbox/authorizations/hubspot.auth.json" )
 };
 const lager = require( "properjs-lager" );
-const validator = require( "validator" );
+const validator = {
+    isEmpty ( str ) {
+        return (str !== "");
+    },
+    isEmail ( str ) {
+        return /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test( str );
+    }
+};
 
 
 
